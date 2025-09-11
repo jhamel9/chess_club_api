8a5100b100f36d4484c4a59e5f3b2662c805225c
greggcaburnay56 <greggcaburnay56@gmail.com>
2025-09-11 11:50:50 +0800

    Update googleSheets.js

--- config/googleSheets.js
+++ config/googleSheets.js
@@ -1,1 +1,15 @@
-.
+// /config/googleSheets.js
+const { google } = require('googleapis');
+require('dotenv').config();
+
+// Create auth client
+const auth = new google.auth.GoogleAuth({
+  keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
+
+// Create the configured Sheets API client
+const sheets = google.sheets({ version: 'v4', auth });
+
+// Export the configured sheets client
+module.exports = sheets;
