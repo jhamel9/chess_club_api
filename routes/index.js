de9e73b9fe5daa72cd3c48c5faad0062c89f81cc
.domingsss <cjr3423@gmail.com>
2025-09-11 19:39:52 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,14 @@
-.
+// /routes/index.js
+const express = require('express');
+const router = express.Router();
+
+// Import route modules
+const clubRoutes = require('./clubRoutes');
+
+// Use route modules
+router.use('/clubs', clubRoutes);
+
+// Add more routes here later (tournamentRoutes, etc.)
+// router.use('/tournaments', tournamentRoutes);
+
+module.exports = router;
